11d623b463391d6f5f7f9962ff7dcc26a0632ae7
Artyom Danko <cflabravo@yahoo.com>
2022-12-10 15:05:53 +0300

    feat: Connect ReactIntl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Components/Header.js'
+import { IntlProvider } from 'react-intl';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <IntlProvider messages={{}} locale='ru' defaultLocale='ru'>
+      <div>
+        <Header />
+      </div>
+    </IntlProvider>
+
   );
 }
 

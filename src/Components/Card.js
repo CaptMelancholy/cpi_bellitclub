40b47d62f50109a96abb4d46078efad69fbcf17b
Artyom Danko <cflabravo@yahoo.com>
2022-12-13 21:26:02 +0300

    feat: design update

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,20 @@
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
-function Card({person}) {
-    return(
+function Card({ person }) {
+  return (
+    <div>
+      <img class="card-image" alt={person.name} src={person.imgPath} />
       <div>
-        <img class="card-image" alt={person.name} src={person.imgPath} />
-        <div>
-          <h2>{person.name}</h2>
-        </div>
-        <button class="card-button"><Link to={person.url} class="card-button_text">ИНФОРМАЦИЯ</Link></button>
+        <h2>{person.name}</h2>
       </div>
-    );
-  }
-  
-  export default Card;
+      <Link to={person.url}>
+        <Button className='text-center btn btn-light border border-5' style={{ width: '15rem' }}>
+          <span className='me-auto text-secondary'>ИНФОРМАЦИЯ</span>
+        </Button>
+      </Link>
+    </div>
+  );
+}
+
+export default Card;

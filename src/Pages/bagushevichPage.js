685d530e70d82df113c3062fe3727b1fa4dcc298
Artyom Danko <cflabravo@yahoo.com>
2022-12-13 13:59:25 +0300

    feat: Photos Section

--- src/Pages/bagushevichPage.js
+++ src/Pages/bagushevichPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Carousel, CarouselItem } from 'react-bootstrap';
 import 'react-vertical-timeline-component/style.min.css';
 import BgTimeLine from '../Components/Authors/Bagushevich/bagushevichTimeLine.js';
 
@@ -7,6 +8,18 @@ export default class bagushevichPage extends Component {
         return (
         <>
            <BgTimeLine /> 
+           <Carousel variant="dark" className='w-75 d-flex t-center'>
+                <CarouselItem >
+                    <img className="d-block w-75 m-auto" src='https://www.nlb.by/upload/iblock/21b/af.jpg' alt='books' />
+                    
+                </CarouselItem>
+                <CarouselItem>
+                <img className="d-block w-75 m-auto" src='https://libcat.ru/uploads/posts/pictures/187428-bahusevic_tvory_apisannie_630.jpg' alt='books' />
+                </CarouselItem>
+                <CarouselItem>
+                <img className="d-block w-75 m-auto" src='https://www.nlb.by/upload/medialibrary/7bf/bahushevich.jpg' alt='books' />
+                </CarouselItem>
+           </Carousel>
         </>
             
         )

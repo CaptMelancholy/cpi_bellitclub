a5a3f06f06369ad11944699dbfefaf013da39980
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2022-12-15 18:24:30 +0300

    hot fix: card-container positioning

--- src/Components/AuCard.js
+++ src/Components/AuCard.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 const AuCard = ({ element }) => (
-    <div style={ {display: 'flex', justifyContent: 'center', textAlign: 'center'} }>
-        <div className='card-author w-50'>
+    <div class="card-container" style={ {display: 'flex', justifyContent: 'center', textAlign: 'center'} }>
+        <div className='card-author'>
             <img className='card-image' src={element.imgPath} alt='author'></img>
             <h1>{element.name}</h1>
             <h2>{element.date}</h2>
             <p>{element.details}</p>
         </div>
-
     </div>
 )
 
